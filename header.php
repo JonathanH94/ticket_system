e23a2751828ba6f6610fcfdd69fabfabb244c9c0
JonathanH94 <jono_125@yahoo.co.uk>
2024-08-15 22:53:15 +0100

    Created navbar and css style. Connected to the database

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Navbar</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    
+    <nav class="navbar">
+        <div>
+            <h1>Ticketing System</h1>
+            <ul>
+                <li><a href="">My Tickets</a></li>
+                <li><a href="">Home</a></li>
+
+            </ul>
+        
+        </div>
+    </nav>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require('header.php')
+
+?>
+
+<div>
+
+    <h2 class="index">My Tickets</h2>
+
+    <hr/>
+
+
+</div>
